18db52479c7d338cc523a48b7fb4a63806e012b9
Paulo <pc5783346@gmail.com>
2023-01-30 15:02:08 -0300

    feat: returns 409 httpstatus when creating a user with already registered email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
+  login(){
+    return 'Login done';
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from '../prisma/prisma.service';
@@ -10,11 +10,17 @@ export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<void> {
+    const userEmail = await this.prisma.users.findUnique({
+      where: { email: createUserDto.email }
+    });
+
+    if(userEmail) throw new ConflictException();
+
     const data: Prisma.UsersCreateInput = {
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10)
     };
 
-    await this.prisma.users.create({data})
+    await this.prisma.users.create({data});
   }
 }
