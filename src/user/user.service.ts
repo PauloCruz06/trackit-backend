876aa7df55192bfbfb77e4c412fba3634218f605
Paulo <pc5783346@gmail.com>
2023-01-30 13:53:14 -0300

    doc: create auth endpoint files

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,22 +17,4 @@ export class UserService {
 
     await this.prisma.users.create({data})
   }
-
-  /*
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
-  */
 }
